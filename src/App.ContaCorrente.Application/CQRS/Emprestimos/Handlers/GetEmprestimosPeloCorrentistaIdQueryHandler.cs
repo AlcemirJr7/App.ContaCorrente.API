f91bff56d228953bd4ec73ba84763d173f546f8b
AlcemirJr7 <alcemir.jr7@gmail.com>
2022-05-25 13:28:37 -0300

    implementação emprestimo
    implementação emprestimo

---
+++ src/App.ContaCorrente.Application/CQRS/Emprestimos/Handlers/GetEmprestimosPeloCorrentistaIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using App.ContaCorrente.Application.CQRS.Emprestimos.Queries;
+using App.ContaCorrente.Domain.Entidades;
+using App.ContaCorrente.Domain.Interfaces;
+using App.ContaCorrente.Domain.Mensagem;
+using App.ContaCorrente.Domain.Validacoes;
+using MediatR;
+
+namespace App.ContaCorrente.Application.CQRS.Emprestimos.Handlers
+{
+    public class GetEmprestimosPeloCorrentistaIdQueryHandler : IRequestHandler<GetEmprestimosPeloCorrentistaIdQuery, IEnumerable<Emprestimo>>
+    {
+        private readonly IEmprestimoRepositorio _emprestimoRepositorio;
+        public GetEmprestimosPeloCorrentistaIdQueryHandler(IEmprestimoRepositorio emprestimoRepositorio)
+        {
+            _emprestimoRepositorio = emprestimoRepositorio;
+        }
+
+        public async Task<IEnumerable<Emprestimo>> Handle(GetEmprestimosPeloCorrentistaIdQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await _emprestimoRepositorio.GetPeloCorrentistaIdAsync(request.Id);
+            }
+            catch 
+            {
+                throw new DomainException(Mensagens.ErroAoEfetuarConsulta);
+            }
+        }
+    }
+}
---
+++ src/App.ContaCorrente.Application/CQRS/Emprestimos/Queries/GetEmprestimoPeloIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using App.ContaCorrente.Domain.Entidades;
+using MediatR;
+
+namespace App.ContaCorrente.Application.CQRS.Emprestimos.Queries
+{
+    public class GetEmprestimoPeloIdQuery : IRequest<Emprestimo>
+    {
+        public int Id { get; set; }
+        public GetEmprestimoPeloIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
