95ba6d8589420a54d55d3f9fded1dc50e6b8d209
AlcemirJr7 <alcemir.jr7@gmail.com>
2022-05-28 10:32:36 -0300

    Ajuste Jobs e efetivação emprestimo
    Ajuste Jobs e efetivação emprestimo

---
+++ src/App.ContaCorrente.Job/InjecaoDependenciaJob.cs
@@ -0,0 +1,59 @@
+﻿using App.ContaCorrente.Application.Mapeamentos;
+using App.ContaCorrente.Application.Servicos;
+using App.ContaCorrente.Application.Servicos.Interfaces;
+using App.ContaCorrente.Domain.Interfaces;
+using App.ContaCorrente.Infra.Data.Contexto;
+using App.ContaCorrente.Infra.Data.Repositorios;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.ContaCorrente.Job { 
+    public static class InjecaoDependenciaJob
+    {        
+        public static IServiceCollection AddInfraestruturaJob(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<AppDbContexto>(options =>           
+            options.UseSqlServer(configuration.GetConnectionString("DataBase"
+            ), b => b.MigrationsAssembly(typeof(AppDbContexto).Assembly.FullName)));
+
+            //repositorios
+            services.AddScoped<IBancoRepositorio, BancoRepositorio>();
+            services.AddScoped<IEnderecoRepositorio, EnderecoRepositorio>();
+            services.AddScoped<IHistoricoRepositorio, HistoricoRepositorio>();
+            services.AddScoped<ILocalTrabalhoRepositorio, LocalTrabalhoRepositorio>();
+            services.AddScoped<IPessoaRepositorio, PessoaRepositorio>();
+            services.AddScoped<ICorrentistaRepositorio, CorrentistaRepositorio>();
+            services.AddScoped<ISaldoContaCorrenteRepositorio, SaldoContaCorrenteRepositorio>();
+            services.AddScoped<ILancamentoRepositorio, LancamentoRepositorio>();
+            services.AddScoped<IPagamentoRepositorio, PagamentoRepositorio>();
+            services.AddScoped<ILancamentoFuturoRepositorio, LancamentoFuturoRepositorio>();
+            services.AddScoped<IEmprestimoRepositorio, EmprestimoRepositorio>();
+            services.AddScoped<IParcelasEmprestimoRepositorio, ParcelasEmprestimoRepositorio>();
+
+
+            // serviços
+            services.AddScoped<IBancoServico, BancoServico>();
+            services.AddScoped<IEnderecoServico, EnderecoServico>();
+            services.AddScoped<IHistoricoServico, HistoricoServico>();
+            services.AddScoped<ILocalTrabalhoServico, LocalTrabalhoServico>();
+            services.AddScoped<IPessoaServico, PessoaServico>();
+            services.AddScoped<ICorrentistaServico, CorrentistaServico>();
+            services.AddScoped<ISaldoContaCorrenteServico, SaldoContaCorrenteServico>();
+            services.AddScoped<ILancamentoServico, LancamentoServico>();
+            services.AddScoped<IPagamentoServico, PagamentoServico>();
+            services.AddScoped<ILancamentoFuturoServico, LancamentoFuturoServico>();
+            services.AddScoped<IEmprestimoServico, EmprestimoServico>();
+            services.AddScoped<IParcelasEmprestimoServico, ParcelasEmprestimoServico>();
+
+
+            var myHandlers = AppDomain.CurrentDomain.Load("App.ContaCorrente.Application");
+            services.AddMediatR(myHandlers);
+
+            services.AddAutoMapper(typeof(DomainToDTOMapeamentoProfile));
+            services.AddAutoMapper(typeof(DTOToCommandMapeamentoProfile));
+            
+            return services;
+        }
+    }
+}
--- src/App.ContaCorrente.Job/Program.cs
+++ src/App.ContaCorrente.Job/Program.cs
@@ -6,65 +6,45 @@ using App.ContaCorrente.Infra.Data.Contexto;
 using App.ContaCorrente.Infra.Data.Repositorios;
 using App.ContaCorrente.Infra.IoC;
 using App.ContaCorrente.Job;
+using App.ContaCorrente.Job.Jobs;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Quartz;
+
+var builder = WebApplication.CreateBuilder(args);
 
 
 IHost host = Host.CreateDefaultBuilder()
     .ConfigureServices((hostContext, services) =>
     {
-        services.AddDbContext<AppDbContexto>(options =>
-           options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBContaCorrente; Integrated Security=True", b => b.MigrationsAssembly(typeof(AppDbContexto).Assembly.FullName)));
-
-        //repositorios
-        services.AddScoped<IBancoRepositorio, BancoRepositorio>();
-        services.AddScoped<IEnderecoRepositorio, EnderecoRepositorio>();
-        services.AddScoped<IHistoricoRepositorio, HistoricoRepositorio>();
-        services.AddScoped<ILocalTrabalhoRepositorio, LocalTrabalhoRepositorio>();
-        services.AddScoped<IPessoaRepositorio, PessoaRepositorio>();
-        services.AddScoped<ICorrentistaRepositorio, CorrentistaRepositorio>();
-        services.AddScoped<ISaldoContaCorrenteRepositorio, SaldoContaCorrenteRepositorio>();
-        services.AddScoped<ILancamentoRepositorio, LancamentoRepositorio>();
-        services.AddScoped<IPagamentoRepositorio, PagamentoRepositorio>();
-        services.AddScoped<ILancamentoFuturoRepositorio, LancamentoFuturoRepositorio>();
-        services.AddScoped<IEmprestimoRepositorio, EmprestimoRepositorio>();
-        services.AddScoped<IParcelasEmprestimoRepositorio, ParcelasEmprestimoRepositorio>();
-
-
-        // serviços
-        services.AddScoped<IBancoServico, BancoServico>();
-        services.AddScoped<IEnderecoServico, EnderecoServico>();
-        services.AddScoped<IHistoricoServico, HistoricoServico>();
-        services.AddScoped<ILocalTrabalhoServico, LocalTrabalhoServico>();
-        services.AddScoped<IPessoaServico, PessoaServico>();
-        services.AddScoped<ICorrentistaServico, CorrentistaServico>();
-        services.AddScoped<ISaldoContaCorrenteServico, SaldoContaCorrenteServico>();
-        services.AddScoped<ILancamentoServico, LancamentoServico>();
-        services.AddScoped<IPagamentoServico, PagamentoServico>();
-        services.AddScoped<ILancamentoFuturoServico, LancamentoFuturoServico>();
-        services.AddScoped<IEmprestimoServico, EmprestimoServico>();
-        services.AddScoped<IParcelasEmprestimoServico, ParcelasEmprestimoServico>();
-
-        var myHandlers = AppDomain.CurrentDomain.Load("App.ContaCorrente.Application");
-        services.AddMediatR(myHandlers);
-
-        services.AddAutoMapper(typeof(DomainToDTOMapeamentoProfile));
-        services.AddAutoMapper(typeof(DTOToCommandMapeamentoProfile));
-
+        services.AddInfraestruturaJob(builder.Configuration);
+        
         services.AddQuartz(q =>
         {
-            q.UseMicrosoftDependencyInjectionScopedJobFactory();
+            q.UseMicrosoftDependencyInjectionJobFactory();
 
-            var jobKey = new JobKey(Guid.NewGuid().ToString());
+            //Job Lançamentos Futuros
+            var jobKeyLancamentosFuturos = new JobKey(Guid.NewGuid().ToString()  + "LancamentosFuturos");
 
-            q.AddJob<MyJob>(o => o.WithIdentity(jobKey));
+            q.AddJob<ProcessaLancamentosFuturosJob>(o => o.WithIdentity(jobKeyLancamentosFuturos));
 
             q.AddTrigger(o =>
             {
-                o.ForJob(jobKey)
-                .WithIdentity(Guid.NewGuid().ToString())
-                .WithCronSchedule("0 0/1 * * * ?");
+                o.ForJob(jobKeyLancamentosFuturos)
+                .WithIdentity(Guid.NewGuid().ToString() + "LancamentosFuturos")
+                .WithCronSchedule("0 0/1 * * * ?"); // roda a cada 1 min
+            });
+
+            //Job Parcelas Emprestimo
+            var jobKeyParcelasEmprestimo = new JobKey(Guid.NewGuid().ToString() + "ParcelasEmprestimo");
+
+            q.AddJob<ProcessaParcelasEmprestimoJob>(o => o.WithIdentity(jobKeyParcelasEmprestimo));
+
+            q.AddTrigger(o =>
+            {
+                o.ForJob(jobKeyParcelasEmprestimo)
+                .WithIdentity(Guid.NewGuid().ToString() + "ParcelasEmprestimo")
+                .WithCronSchedule("0 0/1 * * * ?"); // roda a cada 1 min
             });
 
             services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
