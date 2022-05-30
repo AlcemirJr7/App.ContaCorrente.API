a29a7147a3272666b82affc2569980f40229a42d
AlcemirJr7 <alcemir.jr7@gmail.com>
2022-05-29 23:20:35 -0300

    Add Correntista na tabela ChavesPix
    Add Correntista na tabela ChavesPix

---
+++ src/App.ContaCorrente.Infra.Data/Migrations/20220530021659_AddCorrentistaChavesPix.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.ContaCorrente.Infra.Data.Migrations
+{
+    public partial class AddCorrentistaChavesPix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CorrentistaId",
+                table: "ChavesPix",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChavesPix_CorrentistaId",
+                table: "ChavesPix",
+                column: "CorrentistaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChavesPix_Correntistas_CorrentistaId",
+                table: "ChavesPix",
+                column: "CorrentistaId",
+                principalTable: "Correntistas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChavesPix_Correntistas_CorrentistaId",
+                table: "ChavesPix");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChavesPix_CorrentistaId",
+                table: "ChavesPix");
+
+            migrationBuilder.DropColumn(
+                name: "CorrentistaId",
+                table: "ChavesPix");
+        }
+    }
+}
